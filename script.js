ff7cb589337e877ed949120f4b3f38ada006b309
SamaraParcero <samaraparceroti@gmail.com>
2024-07-20 12:12:29 -0300

    implementando botões

--- script.js
+++ script.js
@@ -39,3 +39,51 @@ $('.containert').slick({
      }
    ]
  });
+
+
+
+ document.addEventListener('DOMContentLoaded', function () {
+   
+  var modal = document.getElementById("myModal");
+
+  
+  var btn = document.getElementById("modalcontato");
+
+ 
+  var span = document.getElementsByClassName("close")[0];
+
+  
+
+  
+  btn.onclick = function() {
+      modal.style.display = "block";
+      document.body.classList.add('modal-open'); 
+  }
+
+ 
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+
+  function closeModal() {
+      modal.style.display = "none";
+      document.body.classList.remove('modal-open');
+  }
+
+  span.onclick = closeModal;
+
+  window.onclick = function (event) {
+      if (event.target == modal) {
+          closeModal();
+      }
+  }
+
+
+});
